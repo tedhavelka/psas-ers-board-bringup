0c8f5d2638b5f3cb6c2b8a8c2ea2f2cf1e8d35fe
Theodore Havelka <tedhavelka66@gmail.com>
2025-08-28 06:40:52 -0700

    utils: amend openocd wrapper scripts.
    Correct a hard coded path where possible, and for now comment out two shell
    functions which invoke openocd in a manner nearly identical to Zephyr
    4.2.99, but with many arguments to openocd single quoted.
    
    See notes in the wrapper script for details and next steps.

--- samples/hello-world/flash-manually.sh
+++ samples/hello-world/flash-manually.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
+# Note:  (1) Zephyr 4.2.99 flash runner calls openocd this way:
+#
 # /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/bin/openocd -f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/interface/stlink.cfg -f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/target/stm32f0x.cfg -s /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts  '-c init' '-c targets' -c 'reset init' -c 'flash write_image erase /home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex' -c 'reset run' -c shutdown
 
-# In a gdb client session:
-#
-# (gdb) target extended-remote localhost:3333
+# Note:  (2) comment out following two functions, at least until resolving the
+#  preference to expand a shell variable that's presently within single
+#  quotes.  That syntax comes from above Zephyr runner example in note (1).
 
-function flash_only()
-{
-	/opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/bin/openocd \
-	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/interface/stlink.cfg \
-	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/target/stm32f0x.cfg \
-	-s /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts \
-	'-c init' \
-	'-c targets' \
-	-c 'reset init' \
-	-c 'flash write_image erase /home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex' \
-	-c 'reset run' \
-	-c shutdown
-}
+# function flash_only()
+# {
+# 	/opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/bin/openocd \
+# 	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/interface/stlink.cfg \
+# 	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/target/stm32f0x.cfg \
+# 	-s /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts \
+# 	'-c init' \
+# 	'-c targets' \
+# 	-c 'reset init' \
+# 	-c 'flash write_image erase /home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex' \
+# 	-c "flash write_image erase ${PWD}/build/zephyr/zephyr.hex" \
+# 	-c 'reset run' \
+# 	-c shutdown
+# }
 
-function flash_and_enter_debugger()
-{
-	/opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/bin/openocd \
-	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/interface/stlink.cfg \
-	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/target/stm32f0x.cfg \
-	-s /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts \
-	'-c init' \
-	'-c targets' \
-	-c 'reset init' \
-	-c 'flash write_image erase /home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex' \
-	-c 'reset run'
-}
+# function flash_and_enter_debugger()
+# {
+# 	/opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/bin/openocd \
+# 	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/interface/stlink.cfg \
+# 	-f /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts/target/stm32f0x.cfg \
+# 	-s /opt/zephyr-sdk-0.16.5-1/sysroots/x86_64-pokysdk-linux/usr/share/openocd/scripts \
+# 	'-c init' \
+# 	'-c targets' \
+# 	-c 'reset init' \
+# 	-c 'flash write_image erase /home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex' \
+# 	-c 'reset run'
+# }
 
 function flash_with_psas_recovery_board_options()
 {
 	OPENOCD_CONFIG_PATH=/home/ted/projects/psas/psas-avionics/lv3.1-recovery/controlSystem/RecoveryBoard/firmware/toolchain
 	OPENOCD_CONFIG_FILE=oocd.cfg
-        FIRMWARE_IMAGE=/home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex
+        FIRMWARE_IMAGE=${PWD}/build/zephyr/zephyr.hex
 	openocd \
         -f ${OPENOCD_CONFIG_PATH}/${OPENOCD_CONFIG_FILE} \
         -c "program ${FIRMWARE_IMAGE} verify reset exit"
@@ -47,7 +50,7 @@ function flash_with_psas_options_and_debug()
 {
 	OPENOCD_CONFIG_PATH=/home/ted/projects/psas/psas-avionics/lv3.1-recovery/controlSystem/RecoveryBoard/firmware/toolchain
 	OPENOCD_CONFIG_FILE=oocd.cfg
-        FIRMWARE_IMAGE=/home/ted/projects/zephyr-project/psas-ers-firmware/samples/hello-world/build/zephyr/zephyr.hex
+        FIRMWARE_IMAGE=${PWD}/build/zephyr/zephyr.hex
 	openocd \
         -f ${OPENOCD_CONFIG_PATH}/${OPENOCD_CONFIG_FILE} \
         -c "program ${FIRMWARE_IMAGE} verify reset"
@@ -70,11 +73,13 @@ echo "Zephyr openocd flash wrapper starting"
 echo "current dir is $PWD"
 
 if [ "$1" == "w" ]; then
-    echo "Calling openocd with options to flash and then exit openocd . . ."
-    flash_only
+#    echo "Calling openocd with options to flash and then exit openocd . . ."
+#    flash_only
+    flash_with_psas_recovery_board_options
 elif [ "$1" == "d" ]; then
-    echo "Calling openocd with options to flash, start and maintain a gdb server . . ."
-    flash_and_enter_debugger
+#    echo "Calling openocd with options to flash, start and maintain a gdb server . . ."
+#    flash_and_enter_debugger
+    flash_with_psas_options_and_debug
 elif [[ "$1" == "psas" && $# == 1 ]]; then
     echo "Calling openocd with PSAS options . . ."
     flash_with_psas_recovery_board_options
