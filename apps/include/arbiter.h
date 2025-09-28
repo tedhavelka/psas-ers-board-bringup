462fb2505dfcaec2df22d4c87a50be248214f569
Theodore Havelka <tedhavelka66@gmail.com>
2025-09-28 11:49:27 -0700

    app: arbiter: add module to make decisions based on sensor inputs and CAN . . .
    messages.

---
+++ apps/include/arbiter.h
@@ -0,0 +1,6 @@
+#ifndef ERS_ARBITER_H
+#define ERS_ARBITER_H
+
+int32_t ers_init_arbiter(void);
+
+#endif // ERS_ARBITER_H
---
+++ apps/src/arbiter.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025 Portland State Aerospace Society
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(arbiter, LOG_LEVEL_INF);
+
+#include <ers-can.h>
+#include <ers-dac.h>
+#include <keeper.h>
+
+#define ERS_ARBITER_SLEEP_PER_MS 2000
+
+//----------------------------------------------------------------------
+// - SECTION - file scoped
+//----------------------------------------------------------------------
+
+#define ARBITER_THREAD_STACK_SIZE 512
+#define ARBITER_THREAD_PRIORITY 1
+K_THREAD_STACK_DEFINE(arbiter_thread_stack, ARBITER_THREAD_STACK_SIZE);
+struct k_thread arbiter_thread_data;
+
+//----------------------------------------------------------------------
+// - SECTION - routines
+//----------------------------------------------------------------------
+
+void arbiter_thread_entry(void *arg1, void *arg2, void *arg3)
+{
+        ARG_UNUSED(arg1);
+        ARG_UNUSED(arg2);
+        ARG_UNUSED(arg3);
+
+	while (1)
+	{
+		LOG_INF("M3");
+		k_msleep(ERS_ARBITER_SLEEP_PER_MS);
+	}
+}
+
+int32_t ers_init_arbiter(void)
+{
+	int32_t rc = 0;
+
+	k_tid_t arbiter_tid = k_thread_create(&arbiter_thread_data, arbiter_thread_stack,
+					 K_THREAD_STACK_SIZEOF(arbiter_thread_stack),
+					 arbiter_thread_entry, NULL, NULL, NULL,
+				 ARBITER_THREAD_PRIORITY, 0, K_NO_WAIT);
+	if (!arbiter_tid) {
+		LOG_ERR("ERROR spawning rx thread\n");
+	}
+
+	return rc;
+}
