96fe7fce955e073d95680e756f1ebc9df7378784
Theodore Havelka <tedhavelka66@gmail.com>
2025-09-28 23:37:55 -0700

    app: GPIO: add DTS and source code for digital outputs to motor driver chip.
    Add missing DTS for nodes and node aliases, for ERS output signals deploy1,
    deploy2 and not_motor_ps.

--- apps/include/gpio-in.h
+++ apps/include/gpio-in.h
@@ -3,4 +3,10 @@
 
 int32_t ers_init_gpio_in(void);
 
+int32_t ers_gpios_set_deploy1(const uint32_t value);
+
+int32_t ers_gpios_set_deploy2(const uint32_t value);
+
+int32_t ers_gpios_set_not_motor_ps(const uint32_t value);
+
 #endif // GPIO_IN_H
